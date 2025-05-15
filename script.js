37b7117cc7e9d712e08514f9b103b962e6c9e66e
GeorginaBeese <georgina_beese@outlook.com>
2025-05-15 11:40:27 +0100

    Creating the library cards to display on the user window

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
+let libraryContainer = document.querySelector('.container');
 
+// Creating the cards to show the books
+
+function createLibrary() {
+  for (let i = 0; i <= myLibrary.length; i++) {
+    const readTxt = myLibrary[i].read ? "already read" : "not read yet";
+    
+    const card = document.createElement("div")
+    card.id = "book";
+    libraryContainer.appendChild(card)
+
+    const title = document.createElement("h1");
+    const author = document.createElement("h2");
+    const pages = document.createElement("p");
+    const read = document.createElement("p");
+
+    const titleContent = document.createTextNode(`${myLibrary[i].title} `)
+    const authorContent = document.createTextNode(`${myLibrary[i].author} `)
+    const pagesContent = document.createTextNode(`${myLibrary[i].pages} pages, `)
+    const readContent = document.createTextNode(`${readTxt}`)
+    
+    card.appendChild(title);
+    title.appendChild(titleContent);
+
+    card.appendChild(author);
+    author.appendChild(authorContent);
+
+    card.appendChild(pages);
+    pages.appendChild(pagesContent);
+    
+    card.appendChild(read);
+    read.appendChild(readContent);
+}
+}
 
 const myLibrary = [];
 
@@ -23,5 +57,11 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false)
+addBookToLibrary('The Vegetarian', 'H. Kang', 192, true)
+addBookToLibrary('Martyr!', 'K. Akbar', 331, false)
+addBookToLibrary('Three Women', '	L. Taddeo', 320, true)
+addBookToLibrary('A Waiter in Paris', 'E. Chisholm', 384, false)
 
-console.log(myLibrary)
+
+
+createLibrary()
